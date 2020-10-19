9fb82530109d3a1d33537c6457276d56844c5c14
Juan C. Andreu <andreujuanc@gmail.com>
2020-10-19 20:45:45 +0200

    Updates

---
+++ precache-manifest.3aad343157732de3a5aa48443cb4e52c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a366140671ea3e4ee29f22ffb7d50aab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f8a7c3f5b140730fd42",
+    "url": "/static/css/main.310e7e9b.chunk.css"
+  },
+  {
+    "revision": "f2387aafc96801b6ad90",
+    "url": "/static/js/2.f73cded3.chunk.js"
+  },
+  {
+    "revision": "d576ef9c8d0783fb684cf10d76473333",
+    "url": "/static/js/2.f73cded3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f8a7c3f5b140730fd42",
+    "url": "/static/js/main.d3f39a63.chunk.js"
+  },
+  {
+    "revision": "691dee1c826bf95df388",
+    "url": "/static/js/runtime-main.5721f2f5.js"
+  },
+  {
+    "revision": "f01d172013516bd18bb30286d296ec10",
+    "url": "/static/media/logo.f01d1720.webp"
+  }
+]);
