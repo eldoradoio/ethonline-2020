3fa36c7b0d91bede17afa009ff49b8d32a6ee0fe
Juan C. Andreu <andreujuanc@gmail.com>
2020-10-19 20:50:03 +0200

    Updates

---
+++ precache-manifest.bbe799c850f403084fe9849bf21867aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "446b4a6329b3fb2a30e3d1730f44c692",
+    "url": "/ethonline-2020/index.html"
+  },
+  {
+    "revision": "c81f905389e115862836",
+    "url": "/ethonline-2020/static/css/main.310e7e9b.chunk.css"
+  },
+  {
+    "revision": "f2387aafc96801b6ad90",
+    "url": "/ethonline-2020/static/js/2.f73cded3.chunk.js"
+  },
+  {
+    "revision": "d576ef9c8d0783fb684cf10d76473333",
+    "url": "/ethonline-2020/static/js/2.f73cded3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c81f905389e115862836",
+    "url": "/ethonline-2020/static/js/main.d0c078ed.chunk.js"
+  },
+  {
+    "revision": "39e54782a47c9dd38e1c",
+    "url": "/ethonline-2020/static/js/runtime-main.cef7e369.js"
+  },
+  {
+    "revision": "f01d172013516bd18bb30286d296ec10",
+    "url": "/ethonline-2020/static/media/logo.f01d1720.webp"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3aad343157732de3a5aa48443cb4e52c.js"
+  "/ethonline-2020/precache-manifest.bbe799c850f403084fe9849bf21867aa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ethonline-2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
