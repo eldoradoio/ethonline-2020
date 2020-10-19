4b2ad399407f14301958524181f442f8871fa1c4
Juan C. Andreu <andreujuanc@gmail.com>
2020-10-19 18:05:39 +0000

    formmmated

--- src/contracts/providers/mStable/MStableProvider.sol
+++ src/contracts/providers/mStable/MStableProvider.sol
@@ -12,196 +12,190 @@ import "../../IElDoradoSavingsProvider.sol";
 
 //NOTE APY = ((amount now * 1e18 / amount then) - 1e18) * (secondsInYear / depositLengthInSeconds)
 
-contract MStableProvider is IElDoradoSavingsProvider  {
-    
-    using EnumerableSet for EnumerableSet.AddressSet;
+contract MStableProvider is IElDoradoSavingsProvider {
+	using EnumerableSet for EnumerableSet.AddressSet;
 
-    IMasset private masset;
-    ISavingsContract private savings;
-    IERC20 private mUSD;
-    IMStableHelper private helper;
-    
-    EnumerableSet.AddressSet private _tokens;
-    // User > balance
-    mapping(address => uint256) private depositedBalances;
-    // User > credits
-    mapping(address => uint256) private creditBalances;
+	IMasset private masset;
+	ISavingsContract private savings;
+	IERC20 private mUSD;
+	IMStableHelper private helper;
 
-    uint256 private totalDeposited;
+	EnumerableSet.AddressSet private _tokens;
+	// User > balance
+	mapping(address => uint256) private depositedBalances;
+	// User > credits
+	mapping(address => uint256) private creditBalances;
 
-    constructor(
-        address _masset,
-        ISavingsContract _savings,
-        IMStableHelper _helper
-    ) public {
-        masset = IMasset(_masset);
-        savings = _savings;
-        mUSD = IERC20(_masset);
-        helper = _helper;
-        totalDeposited = 0;
-    }
-    
+	uint256 private totalDeposited;
 
-    function approveToken(address _tokenAddress) external returns(bool) {
-        //TODO: owner only
+	constructor(
+		address _masset,
+		ISavingsContract _savings,
+		IMStableHelper _helper
+	) public {
+		masset = IMasset(_masset);
+		savings = _savings;
+		mUSD = IERC20(_masset);
+		helper = _helper;
+		totalDeposited = 0;
+	}
 
-        IERC20 token = IERC20(_tokenAddress);
-        token.approve(address(masset), uint256(-1));
-        token.approve(address(savings), uint256(-1));
-        mUSD.approve(address(savings), uint256(-1));
+	function approveToken(address _tokenAddress) external returns (bool) {
+		//TODO: owner only
 
-        _tokens.add(_tokenAddress);
+		IERC20 token = IERC20(_tokenAddress);
+		token.approve(address(masset), uint256(-1));
+		token.approve(address(savings), uint256(-1));
+		mUSD.approve(address(savings), uint256(-1));
 
-        return true;
-    }
+		_tokens.add(_tokenAddress);
 
-    function deposit(address account, address _tokenAddress, uint256 _amount) external returns(uint256) {
-        //TODO: owner only
-        //TODO Should not use sender but an from address
-        require(_amount > 0, "Deposit amount must be greater than zero");
+		return true;
+	}
 
-        IERC20 token = IERC20(_tokenAddress);
+	function deposit(
+		address account,
+		address _tokenAddress,
+		uint256 _amount
+	) external returns (uint256) {
+		//TODO: owner only
+		//TODO Should not use sender but an from address
+		require(_amount > 0, "Deposit amount must be greater than zero");
 
-        uint256 balance = token.balanceOf(account);
-        require(_amount <= balance, "Insufficient balance");
+		IERC20 token = IERC20(_tokenAddress);
 
-        // Temp transfer bAsset to this contract
-        token.transferFrom(account, address(this), _amount);
-        // mint basset to get masset
+		uint256 balance = token.balanceOf(account);
+		require(_amount <= balance, "Insufficient balance");
 
-        uint256 massetsMinted = masset.mintTo(
-            _tokenAddress,
-            _amount,
-            address(this)
-        );
+		// Temp transfer bAsset to this contract
+		token.transferFrom(account, address(this), _amount);
+		// mint basset to get masset
 
-        // deposit masset. We save the credits per account
-        creditBalances[account] += savings.depositSavings(massetsMinted);
-        depositedBalances[account] += massetsMinted;
-        totalDeposited += massetsMinted;
+		uint256 massetsMinted = masset.mintTo(_tokenAddress, _amount, address(this));
 
-        return massetsMinted;
-    }
+		// deposit masset. We save the credits per account
+		creditBalances[account] += savings.depositSavings(massetsMinted);
+		depositedBalances[account] += massetsMinted;
+		totalDeposited += massetsMinted;
 
-    function getBalance(address account, address _tokenAddress) external view returns(uint256) {
-        // Notes from mStable person:
-        // The amount of mUSD this user owns at any point in time can then be calculated as creditsIssued * exchangeRate / 1e18;
-        return _getBalance(account);
-    }
+		return massetsMinted;
+	}
 
-    function getDeposited(address account, address _tokenAddress) external view returns (uint256) {
-        return _getDeposited(account);
-    }
+	function getBalance(address account, address _tokenAddress) external view returns (uint256) {
+		// Notes from mStable person:
+		// The amount of mUSD this user owns at any point in time can then be calculated as creditsIssued * exchangeRate / 1e18;
+		return _getBalance(account);
+	}
 
-    function getEarnings(address account, address _tokenAddress) external view returns(uint256) {
-        return _getEarnings(account);
-    }
+	function getDeposited(address account, address _tokenAddress) external view returns (uint256) {
+		return _getDeposited(account);
+	}
 
+	function getEarnings(address account, address _tokenAddress) external view returns (uint256) {
+		return _getEarnings(account);
+	}
 
-    // Redeem + Swap back to basset
-    function withdraw(address account, address _tokenAddress, uint256 _amount) external returns(uint256) {
-        require(_amount > 0, "Must withdraw something");
-        // Get user credits
-        uint256 creditUnits = helper.getSaveRedeemInput(savings, _amount);
-        require(
-            creditUnits <= creditBalances[account],
-            "Cannot withdraw more than total in account"
-        );
+	// Redeem + Swap back to basset
+	function withdraw(
+		address account,
+		address _tokenAddress,
+		uint256 _amount
+	) external returns (uint256) {
+		require(_amount > 0, "Must withdraw something");
+		// Get user credits
+		uint256 creditUnits = helper.getSaveRedeemInput(savings, _amount);
+		require(creditUnits <= creditBalances[account], "Cannot withdraw more than total in account");
 
-        // Credits to mAssset
-        uint256 massetReturned = savings.redeem(creditUnits);
+		// Credits to mAssset
+		uint256 massetReturned = savings.redeem(creditUnits);
 
-        bool valid;
-        string memory reason;
-        uint256 bassetUnits;
-        uint256 bassetQuantityArg;
-        // mAsset to bAsset
-        (valid, reason, bassetUnits, bassetQuantityArg) = helper
-            .getRedeemValidity(address(masset), massetReturned, _tokenAddress);
-        uint256 redeemed = masset.redeemTo(
-            address(_tokenAddress),
-            bassetQuantityArg,
-            account
-        );
+		bool valid;
+		string memory reason;
+		uint256 bassetUnits;
+		uint256 bassetQuantityArg;
+		// mAsset to bAsset
+		(valid, reason, bassetUnits, bassetQuantityArg) = helper.getRedeemValidity(address(masset), massetReturned, _tokenAddress);
+		uint256 redeemed = masset.redeemTo(address(_tokenAddress), bassetQuantityArg, account);
 
-        // Recalcualte creditBalances
-        creditBalances[account] -= creditUnits;
-        depositedBalances[account] -= massetReturned;
-        totalDeposited -= massetReturned;
+		// Recalcualte creditBalances
+		creditBalances[account] -= creditUnits;
+		depositedBalances[account] -= massetReturned;
+		totalDeposited -= massetReturned;
 
-        return redeemed;
-    }
+		return redeemed;
+	}
 
-    function exchangeRate() external view returns (uint256) {
-        return savings.exchangeRate();
-    }
+	function exchangeRate() external view returns (uint256) {
+		return savings.exchangeRate();
+	}
 
-    function suggestMintAsset()
-        external
-        view
-        returns (
-            bool,
-            string memory,
-            address
-        )
-    {
-        return helper.suggestMintAsset(address(masset));
-    }
+	function suggestMintAsset()
+		external
+		view
+		returns (
+			bool,
+			string memory,
+			address
+		)
+	{
+		return helper.suggestMintAsset(address(masset));
+	}
 
-    function getTotalDeposited() external view returns (uint256){
-        return totalDeposited;
-    }
+	function getTotalDeposited() external view returns (uint256) {
+		return totalDeposited;
+	}
 
-    function getTotalEarnings() external view returns (uint256) {
-        uint256 balance = helper.getSaveBalance(savings, address(this));
-        if (balance <= totalDeposited) return 0;
-        return balance - totalDeposited;
-    }
+	function getTotalEarnings() external view returns (uint256) {
+		uint256 balance = helper.getSaveBalance(savings, address(this));
+		if (balance <= totalDeposited) return 0;
+		return balance - totalDeposited;
+	}
 
-    function _getEarnings(address account) private view returns(uint256) {
-        uint256 balance = _getBalance(account);
-        uint256 deposited = _getDeposited(account);
-        if (balance <= deposited) return 0;
-        return balance - deposited;
-    }
+	function _getEarnings(address account) private view returns (uint256) {
+		uint256 balance = _getBalance(account);
+		uint256 deposited = _getDeposited(account);
+		if (balance <= deposited) return 0;
+		return balance - deposited;
+	}
 
-    function _getBalance(address account) private view returns(uint256) {
-        // Notes from mStable person:
-        // The amount of mUSD this user owns at any point in time can then be calculated as creditsIssued * exchangeRate / 1e18;
-        return creditBalances[account] * savings.exchangeRate() / 1e18;
-    }
+	function _getBalance(address account) private view returns (uint256) {
+		// Notes from mStable person:
+		// The amount of mUSD this user owns at any point in time can then be calculated as creditsIssued * exchangeRate / 1e18;
+		return (creditBalances[account] * savings.exchangeRate()) / 1e18;
+	}
 
-    function _getDeposited(address account) private view returns(uint256) {
-        return depositedBalances[account];
-    }
+	function _getDeposited(address account) private view returns (uint256) {
+		return depositedBalances[account];
+	}
 
+	function getListOfDepositableTokens() external view returns (address[] memory) {
+		return _getTokens();
+	}
 
-    function getListOfDepositableTokens() external view returns(address[] memory){
-        return _getTokens();
-    }
+	function getListOfWithdrawableTokens() external view returns (address[] memory) {
+		return _getTokens();
+	}
 
-    function getListOfWithdrawableTokens() external view returns(address[] memory){
-        return _getTokens();
-    }
+	function _getTokens() private view returns (address[] memory) {
+		uint256 size = _tokens.length();
+		address[] memory result = new address[](size);
+		for (uint256 i = 0; i < size; i++) {
+			result[i] = _tokens.get(i);
+		}
+		return result;
+	}
 
-    function _getTokens() private view returns (address[] memory) {
-        uint256 size = _tokens.length();
-        address[] memory result = new address[](size);
-        for (uint256 i = 0; i < size; i++) {
-            result[i] = _tokens.get(i);
-        }
-        return result;
-    }
-    // function creditBalances(address) external view returns (uint256);
+	// function creditBalances(address) external view returns (uint256);
 
+	function getProviderId() external view returns (bytes32) {
+		return keccak256("MSTABLE");
+	}
 
-    function getProviderId() external view returns (bytes32) {
-        return keccak256('MSTABLE');
-    }
-    function getProviderVersion() external view returns (uint256){
-        return 1;
-    }
-    function getProviderName() external view returns (string memory){
-        return "mStable";
-    }
+	function getProviderVersion() external view returns (uint256) {
+		return 1;
+	}
+
+	function getProviderName() external view returns (string memory) {
+		return "mStable";
+	}
 }
